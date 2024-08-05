71a6037bc96033c6e47fc59d8411eee4d16670db
MARCOS NAHUEL ALBORNOZ <69739045+MarcosNahuel@users.noreply.github.com>
2024-08-04 21:38:47 -0300

    ULTIMO

--- js/script.js
+++ js/script.js
@@ -1,22 +1,5 @@
-/*==================== toggle icon navbar ====================*/
-let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
-
-
-/*==================== scroll sections active link ====================*/
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
-
-window.onscroll = () => {
-
-
-    /*==================== sticky navbar ====================*/
-
-    /*==================== remove toggle icon and navbar when click navbar link (scroll) ====================*/
-};
-
-
-/*==================== scroll reveal ====================*/
-
-
-/*==================== typed js ====================*/
+// Puedes añadir funcionalidades interactivas aquí
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('El sitio está listo.');
+});
+=================*/
